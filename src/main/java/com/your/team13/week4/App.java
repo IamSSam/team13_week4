d50652eaae4f3786ff4c2ee2b4fe3ecb7cbf9a51
sangjun2 <sj27294626@gmail.com>
2016-03-31 19:22:19 +0900

    issues 재수정

--- src/main/java/com/your/team13/week4/App.java
+++ src/main/java/com/your/team13/week4/App.java
@@ -12,14 +12,11 @@ public class App {
 
 	}
 
-	public static void main(String[] args) {
-		
-		
+	public static void main(String[] args) {		
 		String inputPlan;
 		int calltime;
 		int lineNumber;
 
-		
 		Plan plan = null;
 		Calculate calculate;
 		Customer customer;
@@ -27,10 +24,6 @@ public class App {
 		Scanner sc = new Scanner(System.in);
 
 		logger.log(Level.INFO, " Plan : ");
-		
-		PrintStream out = System.out;
-		out.print("Plan : ");
-		
 		inputPlan = sc.next().toLowerCase();
 
 		if ("gold".equals(inputPlan)) {
@@ -39,17 +32,17 @@ public class App {
 			plan = new Silver();
 		}
 
-		System.out.print("총사용시간 : ");
+		logger.log(Level.INFO, " 총 사용시간 : ");
 		calltime = sc.nextInt();
 
-		System.out.print("사용회선 : ");
+		logger.log(Level.INFO, " 사용회선 : ");
 		lineNumber = sc.nextInt();
 
 		sc.close();
 
 		customer = new Customer(plan, calltime, lineNumber);
 		calculate = new Calculate(customer);
-
+		
 		System.out.printf("금액 : %.2f", calculate.totalRate());
 	}
 }
